a7e4ec23255b8b02a05ab218717c13681f347d0d
subham <sapkotasubham4@gmail.com>
2026-06-18 10:30:42 +0545

    Tyoes of operators in js

---
+++ Arithmetic.js
@@ -0,0 +1,22 @@
+
+
+
+
+console.log(10 + 5);  
+
+console.log(10 - 5);  
+
+console.log(10 * 5);  
+
+console.log(10 / 5); 
+
+console.log(10 % 3);  
+
+console.log(2 ** 3);  
+
+let count = 5;
+
+count++; 
+
+console.log(count);  
+
---
+++ Comparison.js
@@ -0,0 +1,34 @@
+
+
+console.log(5 == "5");   
+
+console.log(5 === "5");  
+
+console.log(5 != "6");   
+
+console.log(5 !== "5");  
+
+console.log(10 > 5);     
+
+console.log(3 <= 3);   
+
+let x = 10;  
+
+x += 5;       
+
+x -= 3;      
+
+x *= 2;      
+
+x /= 4;       
+
+x %= 4;     
+
+x **= 3;
+
+
+
+
+
+px
+Hide Ads
